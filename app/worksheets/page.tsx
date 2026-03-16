4277f2238bb374d107af88eaa0b6bebd0fa1d587
Ethan Barath <ethanbarath01@gmail.com>
2026-03-16 18:51:21 -0400

    add worksheets index page

---
+++ app/worksheets/page.tsx
@@ -0,0 +1,17 @@
+export default function Worksheets(){
+return(
+<div style={{padding:"40px",fontFamily:"sans-serif"}}>
+<h1>Math Worksheets</h1>
+<p>Select a worksheet below.</p>
+
+<ul>
+<li>
+<a href="/worksheets/fractions-grade-6">
+6th Grade Fractions Worksheet
+</a>
+</li>
+</ul>
+
+</div>
+)
+}
